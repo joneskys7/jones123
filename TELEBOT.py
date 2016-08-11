c4627fbf41e1e054284f4a224354afa7d3a40198
SOLOMON JONES <joneskys7@users.noreply.github.com>
2016-08-11 10:37:13 +0530

    Add files via upload

---
+++ TELEBOT.py
@@ -0,0 +1,18 @@
+1. Install telegram on mobile phone
+
+2. Text to Botfather in search option.
+
+3. Install telepot on raspberry pi.
+
+    sudo apt-get install python-pip
+    sudo pip install telebot
+
+4.Text Token
+
+    import teleort
+    bot = telebot.Bot('*** copy bot token from browser ***')
+    bot.getMe()
+
+5. Type a code in editor and save it with some filename.py
+
+6. Run and Text i
