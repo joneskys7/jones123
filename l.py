55833a3359a449fe96ff1684f30c10d0de215ba4
SOLOMON JONES <joneskys7@users.noreply.github.com>
2016-09-20 22:21:16 +0530

    Add files via upload

---
+++ l.py
@@ -0,0 +1,10 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7,GPIO.OUT)
+while True:
+	GPIO.output(7,1)
+	time.sleep(0.0015)
+	GPIO.output(7,0)
+	time.sleep(0.0025)
+GPIO.cleanup()
---
+++ servo.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7,GPIO.OUT)
+p=GPIO.PWM(7,50)
+p.start(7.5)
+while True:
+	p.ChangeDutyCycle(7.5)
+	time.sleep(1)
+	
+	p.ChangeDutyCycle(2.5)
+	time.sleep(1)
+p.stop()
+GPIO.cleanup()
