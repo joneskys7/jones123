e81e7c42496b7c39f2f125c2e225aaeef0e16fe6
SOLOMON JONES <joneskys7@users.noreply.github.com>
2017-10-28 11:04:19 +0000

    Add files via upload

---
+++ auto terminal.py
@@ -0,0 +1,30 @@
+
+
+cd ~
+
+nano .config/lxsession/LXDE-pi/autostart
+
+add this to end of file:
+
+@lxterminal
+
+
+
+
+
+
+for autorun python file after open
+
+sudo nano .bashrc
+
+(go to last after "fi" write command)
+
+sudo python filename.py
+
+
+ctrl+x
+
+y
+
+Enter
+
